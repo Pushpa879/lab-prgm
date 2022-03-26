9236907ddcf923aeda0a3deeff3e43ff8789a308
PUSHPAS2 <>
2022-03-26 04:03:00 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@lab-prgm/main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int btod (int);
+int main()
+{
+  int binary,decimal;
+  printf("enter the binary");
+     scanf("%d",& binary);
+  decimal=btod(binary);
+  printf("decimal equivalent=%d\n",decimal);
+  return 0;
+  }
+int btod(int bin)
+  {
+
+
+      
+      
+
+  
+
+
+      
+    
